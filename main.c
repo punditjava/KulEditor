6bf48875e28a769c1055d5ace2ca897dc5174089
Andrey <andr67890@gmail.com>
2019-10-07 16:59:29 +0300

    Доработал чтение файла, перемещение по файлу, Вывел вниз статус, номер строчки и гор. клавиши. Сейчас приступаю к редактированию файла(изменить текст, сохранить файл, создать новый)

--- main.c
+++ main.c
@@ -7,16 +7,19 @@
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
-#include <termios.h>
-#include <unistd.h>
-#include <string.h>
+#include <stdarg.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <sys/types.h>
+#include <termios.h>
+#include <time.h>
+#include <unistd.h>
 
 	/***DEFINES***/
 #define CTRL_KEY(k) ((k) & 0x1f)
-#define KUL_VERSION "0.2.0"
+#define KUL_VERSION "0.3.0"
+#define TAB_STOP 8
 #define ABUF_INIT {NULL, 0}
 
 enum editor_key{
@@ -35,16 +38,24 @@ enum editor_key{
 
 typedef struct erow{
 	int size;
+	int rsize;/*размер для вкладок*/
 	char *chars;
+	char *render;
 } erow;
 
 struct config
 {
 	int cx, cy; /*Координаты курсора*/
+	int rx;
+	int scroll_ver;
+	int scroll_hor;
 	int screenrows;
 	int screencols;
 	int num_row;
 	erow *row;
+	char *file_name;
+	char status_message[80];
+	time_t status_time;
 	struct termios global_term; /*Original terminal*/	
 };
 
@@ -118,8 +129,8 @@ int read_key()
   		} else{
   			switch (seq[1]){
   				case 'A' : return ARROW_UP;
-  				case 'B' : return ARROW_UP;
-  				case 'C' : return ARROW_UP;
+  				case 'B' : return ARROW_DOWN;
+  				case 'C' : return ARROW_RIGHT;
   				case 'D' : return ARROW_LEFT;
   				case 'H' : return HOME_KEY;
   				case 'F' : return END_KEY;
@@ -139,7 +150,7 @@ int read_key()
 
 int cursor_position(int *rows, int *cols)
 {
-	char buf[32];
+	char buf[64];
 	unsigned int i = 0;
 
 	if(write(STDOUT_FILENO, "\x1b[6n", 4) != 4)
@@ -174,6 +185,43 @@ int get_window_size(int *rows, int *cols)
 	}
 }
 	/***ROW OPERATIONS***/
+	
+int row_cx_to_rx(erow *row, int cx)
+{
+	int rx = 0;
+	int j;
+	for (j = 0; j < cx; ++j)
+	{
+		if (row->chars[j] == '\t')
+			rx += (TAB_STOP - 1) - (rx % TAB_STOP);
+		rx++;
+	}
+	return rx;
+}		
+
+void update_row(erow *row)
+{
+	int tabs = 0;
+	int j;
+	for (j = 0; j < row->size; ++j){
+		if (row->chars[j] == '\t') tabs++;
+	}
+
+	free(row->render);
+	row->render = malloc(row->size + tabs*(TAB_STOP - 1) + 1);
+	int dx = 0;
+	for (j = 0; j < row->size; ++j){
+		if (row -> chars[j] == '\t'){
+			row -> render[dx++] = ' ';
+			while(dx % TAB_STOP != 0) row->render[dx++] = ' ';
+		} else {
+			row->render[dx++] = row->chars[j];
+		}
+	}
+	row->render[dx] = '\0';
+	row->rsize = dx;
+}
+
 
 void append_row(char *s, size_t len)
 {	
@@ -184,6 +232,11 @@ void append_row(char *s, size_t len)
 	CONFIG.row[at].chars = malloc(len + 1);
 	memcpy(CONFIG.row[at].chars, s, len);
 	CONFIG.row[at].chars[len] = '\0';
+
+	CONFIG.row[at].rsize = 0;
+	CONFIG.row[at].render = NULL;
+	update_row(&CONFIG.row[at]);
+
 	CONFIG.num_row++;
 	
 }
@@ -192,6 +245,9 @@ void append_row(char *s, size_t len)
 
 void editor_open(char *file_name)
 {
+	free(CONFIG.file_name);
+	CONFIG.file_name = strdup(file_name);/*делает копию этой строки*/
+
 	FILE *fp = fopen(file_name, "r");
 	if(!fp) die("fopen");
 
@@ -234,13 +290,36 @@ void ab_free(struct append_buffer *ab)
 
 	/*** OUTPUT ***/
 
-void drawRows(struct append_buffer *ab)
+void check_scroll()
+{
+	CONFIG.rx = 0;
+	if(CONFIG.cy < CONFIG.num_row){
+		CONFIG.rx = row_cx_to_rx(&CONFIG.row[CONFIG.cy], CONFIG.cx);
+	}
+
+	if(CONFIG.cy < CONFIG.scroll_ver){  /*Делаем так чтобы scroll_ver попал в наш экран*/
+		CONFIG.scroll_ver = CONFIG.cy;
+	}
+	if(CONFIG.cy >= CONFIG.scroll_ver + CONFIG.screenrows){
+		CONFIG.scroll_ver = CONFIG.cy - CONFIG.screenrows + 1;
+	}
+	if(CONFIG.rx < CONFIG.scroll_hor){
+		CONFIG.scroll_hor = CONFIG.rx;
+	}
+	if(CONFIG.rx >= CONFIG.scroll_hor + CONFIG.screencols){
+		CONFIG.scroll_hor = CONFIG.rx - CONFIG.screencols + 1;
+	}
+}
+
+
+void draw_rows(struct append_buffer *ab)
 {
 	int i;
 	for (i = 0; i < CONFIG.screenrows; ++i)
 	{	
-		if(i >= CONFIG.num_row){
-			if (CONFIG.num_row == 0 &&i == CONFIG.screenrows / 3) {
+		int file_scroll_ver = i + CONFIG.scroll_ver;
+		if(file_scroll_ver >= CONFIG.num_row){
+			if (CONFIG.num_row == 0 && i == CONFIG.screenrows / 3) {
 				char welcome[80];
 				int welcome_len = snprintf(welcome, sizeof(welcome),
 					"KulEditor -- version %s", KUL_VERSION);
@@ -256,29 +335,67 @@ void drawRows(struct append_buffer *ab)
 			ab_append(ab, "~", 1); /*Заменить потом на цифры!*/
 			}
 		} else {
-			int len = CONFIG.row[i].size;
+			int len = CONFIG.row[file_scroll_ver].rsize - CONFIG.scroll_hor;
+			if(len < 0) len =0;
 			if(len > CONFIG.screencols) len = CONFIG.screencols; /*Вот тут я обрезал строку(добавить потом режим переноса?)*/
-			ab_append(ab, CONFIG.row[i].chars, len);
+			ab_append(ab, &CONFIG.row[file_scroll_ver].render[CONFIG.scroll_hor], len);
 		}
-
+		
 		ab_append(ab, "\x1b[K", 3);
-		if (i < CONFIG.screenrows - 1)
-		{
-			ab_append(ab, "\r\n", 2);
+		
+		ab_append(ab, "\r\n", 2);
+	
+		
+	}
+}
+
+void draw_status_bar(struct append_buffer *ab)
+{
+	ab_append(ab, "\x1b[7m", 4);
+	char status[80], rstatus[80];
+	int len = snprintf(status, sizeof(status), "%.20s - %d lines", 
+		CONFIG.file_name ? CONFIG.file_name : "[No Name]", CONFIG.num_row);
+	int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d", CONFIG.cy + 1, CONFIG.num_row);
+	if(len > CONFIG.screencols) len = CONFIG.screencols;
+	ab_append(ab, status, len);
+	while (len < CONFIG.screencols)
+	{
+		if(CONFIG.screencols - len == rlen){
+			ab_append(ab, rstatus, rlen);
+			break;
+		} else{
+		ab_append(ab, " ", 1);
+		len++;
 		}
 	}
+	ab_append(ab, "\x1b[m", 3);
+	ab_append(ab, "\r\n", 2);
+}
+
+void draw_status_message_bar(struct append_buffer *ab)
+{
+	ab_append(ab, "\x1b[K", 3);
+	int msg_len = strlen(CONFIG.status_message);
+	if(msg_len > CONFIG.screencols) msg_len = CONFIG.screencols;
+	if(msg_len && time(NULL) - CONFIG.status_time < 5)
+		ab_append(ab, CONFIG.status_message, msg_len);
 }
 
 void refresh_screen()
 {
+	check_scroll();
+
 	struct append_buffer ab = ABUF_INIT;
 	ab_append(&ab, "\x1b[?25l", 6);
 	ab_append(&ab, "\x1b[H", 3);
 
-	drawRows(&ab);
+	draw_rows(&ab);
+	draw_status_bar(&ab);
+	draw_status_message_bar(&ab);
 
 	char buf[32];
-	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", CONFIG.cy + 1, CONFIG.cx + 1);/*Точное положение курсора*/
+	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", CONFIG.cy - CONFIG.scroll_ver + 1, 
+											  CONFIG.rx - CONFIG.scroll_hor + 1);/*Точное положение курсора*/
 	ab_append(&ab, buf, strlen(buf));
 
 	ab_append(&ab, "\x1b[?25h", 6);
@@ -287,19 +404,37 @@ void refresh_screen()
 	ab_free(&ab);
 }
 
+void set_status_message(const char *fmt, ...){
+	va_list ap;
+	va_start(ap, fmt);
+	vsnprintf(CONFIG.status_message, sizeof(CONFIG.status_message), fmt, ap);
+	va_end(ap);
+	CONFIG.status_time = time(NULL);
+}
+
 	/*** INPUT ***/
 void move_cursor(int key)
 {
+	erow *row = (CONFIG.cy >= CONFIG.num_row) ? NULL : &CONFIG.row[CONFIG.cy];
+
 	switch(key){
 
 		case ARROW_LEFT:
 		if(CONFIG.cx != 0) /*Прверка на границы экрана*/
     		CONFIG.cx--;
+    		else if (CONFIG.cy > 0){
+    			CONFIG.cy--;
+    			CONFIG.cx = CONFIG.row[CONFIG.cy].size; /*если налево закончилось идём в конец предыдущей*/
+    		}
        	break;
 
     	case ARROW_RIGHT:
-    	if(CONFIG.cx != CONFIG.screencols - 1)
-      		CONFIG.cx++;
+    	if (row && CONFIG.cx < row->size){
+    		CONFIG.cx++;
+    	} else if(row && CONFIG.cx == row->size){ /*как налево только направо и вниз*/
+    		CONFIG.cy++;
+    		CONFIG.cx = 0;
+    	}
         break;
  
         case ARROW_UP:
@@ -308,10 +443,17 @@ void move_cursor(int key)
         break;
 
         case ARROW_DOWN:
-        if(CONFIG.cy != CONFIG.screenrows - 1)
+        if(CONFIG.cy < CONFIG.num_row)
         	CONFIG.cy++;
         break;
 	}
+
+	row = (CONFIG.cy >= CONFIG.num_row) ? NULL : &CONFIG.row[CONFIG.cy];
+	int row_len = row ? row->size : 0;
+	if(CONFIG.cx > row_len){
+		CONFIG.cx = row_len;
+	}
+
 }
 
 void keypress()
@@ -329,15 +471,19 @@ void keypress()
 		CONFIG.cx = 0;
 		break;
 		case END_KEY:
-		CONFIG.cx = CONFIG.screencols - 1;
+		if(CONFIG.cy < CONFIG.num_row)
+			CONFIG.cx = CONFIG.row[CONFIG.cy].size;
 		break;
 
 		case PAGE_UP:
 		case PAGE_DOWN:
 		{
-			int times = CONFIG.screenrows;
-			while(times --)
-				move_cursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+			if(c == PAGE_UP){
+				CONFIG.cy = CONFIG.scroll_ver;
+			} else if(c == PAGE_DOWN){
+				CONFIG.cy = CONFIG.scroll_ver + CONFIG.screenrows - 1;
+				if(CONFIG.cy > CONFIG.num_row) CONFIG.cy = CONFIG.num_row;
+			}
 		}
 		break;
 
@@ -356,11 +502,18 @@ void init()
 {
 	CONFIG.cx = 0;
 	CONFIG.cy = 0;
+	CONFIG.rx = 0;
+	CONFIG.scroll_ver = 0;
+	CONFIG.scroll_hor = 0;
 	CONFIG.num_row = 0;
 	CONFIG.row = NULL;
+	CONFIG.file_name = NULL;
+	CONFIG.status_message[0] = '\0';
+	CONFIG.status_time = 0;
 
 	if(get_window_size(&CONFIG.screenrows, &CONFIG.screencols) == -1)
 		die("get_window_size");
+	CONFIG.screenrows -= 2;
 }
 
 int main(int argc, char *argv[]) 
@@ -370,6 +523,8 @@ int main(int argc, char *argv[])
 	if(argc >= 2){
 		editor_open(argv[1]);
 	}
+
+	set_status_message("HELP: Ctrl-Q = quit");
 
 	while (1){
 		refresh_screen();
